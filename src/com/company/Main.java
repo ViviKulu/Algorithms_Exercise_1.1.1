b84be0da142b99ef6583666a91ec753c595d2685
Vivian Babirye <babimaji1111@gmail.com>
2018-12-19 22:23:39 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    //    Give the value of each of the following expressions:
+
+//    a. ( 0 + 15 ) / 2
+//    b. 2.0e-6 * 100000000.1
+//    c. true && false || true && true
+
+
+    //    Answers:
+
+//    a. ( 0 + 15 ) / 2 = 7.5
+//    b. 2.0e-6 * 100000000.1 = 1562500.00156
+//    c. true && false || true && true = true
+
+    public static void main(String[] args) {
+        // write your code here
+
+    }
+}
