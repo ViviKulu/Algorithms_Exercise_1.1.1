252c58a3705b7c0c69100eb2c865dcaa1936557f
Vivian Babirye <babimaji1111@gmail.com>
2018-12-19 22:35:08 -0500

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,23 +2,21 @@ package com.company;
 
 public class Main {
 
-    //    Give the type and value of each of the following expressions:
+    //    Give the value of each of the following expressions:
 
-    //    a. (1 + 2.236)/2
-    //    b. 1 + 2 + 3 + 4.0
-    //    c. 4.1 >= 4
-    //    d. 1 + 2 + "3"
+//    a. ( 0 + 15 ) / 2
+//    b. 2.0e-6 * 100000000.1
+//    c. true && false || true && true
 
 
     //    Answers:
 
-    //    a. (1 + 2.236)/2 = 1.618
-    //    b. 1 + 2 + 3 + 4.0 = 10
-    //    c. 4.1 >= 4 = true
-    //    d. 1 + 2 + "3" = 33
-
+//    a. ( 0 + 15 ) / 2 = 7.5
+//    b. 2.0e-6 * 100000000.1 = 1562500.00156
+//    c. true && false || true && true = true
 
     public static void main(String[] args) {
         // write your code here
+
     }
 }
